6451eb411fdda8c8127f8959b669a29c9b83142b
peterviktortoth <152824719+peterviktortoth@users.noreply.github.com>
2024-04-20 17:55:49 -0400

    first deploy

--- service_data_fe/src/App.js
+++ service_data_fe/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { post } from '@aws-amplify/api';
+import { Amplify } from '@aws-amplify/core';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = async () => {
+        const apiName = 'serviceDataAPI'; // The name of the API
+        const path = '/yourResourcePath'; // The path you have configured for your API
+        const init = {
+            body: { content: inputValue }, // The data you want to send
+            headers: {}  // Optional headers
+        };
+
+        try {
+            const response = await post(apiName, path, init);
+            console.log('API response:', response);
+        } catch (err) {
+            console.error('Error calling API:', err);
+        }
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    );
 }
 
 export default App;
